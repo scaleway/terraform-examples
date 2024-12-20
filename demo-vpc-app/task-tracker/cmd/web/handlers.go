1362fe57c8143cd4c6eecb9ca61cef09d458ace2
Yacine FODIL <yfodil@scaleway.com>
2024-12-20 10:35:44 +0100

    initial commit

---
+++ demo-vpc-app/task-tracker/cmd/web/handlers.go
@@ -0,0 +1,113 @@
+package main
+
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/scaleway/terraform-examples/demo-vpc-app/internal/validator"
+)
+
+type taskForm struct {
+	Content             string `form:"content"`
+	validator.Validator `form:"-"`
+}
+
+func (app *appConfig) home(w http.ResponseWriter, r *http.Request) {
+	tasks, err := app.tasks.GetAll()
+	if err != nil {
+		app.serverError(w, r, err)
+		return
+	}
+
+	data := app.newTemplateData(r)
+	data.Tasks = tasks
+	data.Form = taskForm{}
+
+	app.renderTemplate(w, r, http.StatusOK, "home.tmpl", data)
+}
+
+func (app *appConfig) taskCreatePost(w http.ResponseWriter, r *http.Request) {
+	var form taskForm
+
+	err := app.decodeForm(r, &form)
+	if err != nil {
+		app.clientError(w, http.StatusBadRequest)
+		return
+	}
+
+	form.CheckField(validator.NotBlank(form.Content), "content", "This field cannot be blank")
+
+	if !form.Valid() {
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+		return
+	}
+
+	id, err := app.tasks.Insert(form.Content)
+	if err != nil {
+		app.serverError(w, r, err)
+		return
+	}
+
+	if r.Header.Get("HX-Request") == "true" {
+		task, err := app.tasks.Get(id)
+		if err != nil {
+			app.serverError(w, r, err)
+			return
+		}
+		data := app.newTemplateData(r)
+		data.Task = task
+		app.renderTemplate(w, r, http.StatusOK, "task.tmpl", data)
+		return
+	}
+
+	http.Redirect(w, r, "/", http.StatusSeeOther)
+}
+
+func (app *appConfig) taskToggleComplete(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.PathValue("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+
+	err = app.tasks.Complete(id)
+	if err != nil {
+		app.serverError(w, r, err)
+		return
+	}
+
+	if r.Header.Get("HX-Request") == "true" {
+		task, err := app.tasks.Get(id)
+		if err != nil {
+			app.serverError(w, r, err)
+			return
+		}
+		data := app.newTemplateData(r)
+		data.Task = task
+		app.renderTemplate(w, r, http.StatusOK, "task.tmpl", data)
+		return
+	}
+
+	http.Redirect(w, r, "/", http.StatusSeeOther)
+}
+
+func (app *appConfig) taskDelete(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.PathValue("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+
+	err = app.tasks.Delete(id)
+	if err != nil {
+		app.serverError(w, r, err)
+		return
+	}
+
+	if r.Header.Get("HX-Request") == "true" {
+		w.WriteHeader(http.StatusOK)
+		return
+	}
+
+	http.Redirect(w, r, "/", http.StatusSeeOther)
+}
---
+++ demo-vpc-app/task-tracker/cmd/web/main.go
@@ -0,0 +1,122 @@
+package main
+
+import (
+	"context"
+	"database/sql"
+	"errors"
+	"flag"
+	"html/template"
+	"log/slog"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/golang-migrate/migrate/v4"
+	"github.com/golang-migrate/migrate/v4/database/postgres"
+	"github.com/scaleway/terraform-examples/demo-vpc-app/internal/models"
+
+	"github.com/go-playground/form/v4"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+	_ "github.com/lib/pq"
+)
+
+type appConfig struct {
+	logger        *slog.Logger
+	tasks         *models.TaskModel
+	templateCache map[string]*template.Template
+	formDecoder   *form.Decoder
+}
+
+func main() {
+	addr := flag.String("addr", ":4000", "HTTP network address")
+	dsn := getDSN()
+	flag.Parse()
+
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+
+	db, err := connectDB(dsn)
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
+	defer db.Close()
+
+	logger.Info("database connection pool established")
+
+	migrationDriver, err := postgres.WithInstance(db, &postgres.Config{})
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
+
+	migrator, err := migrate.NewWithDatabaseInstance("file://migrations", "postgres", migrationDriver)
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
+
+	err = migrator.Up()
+	if err != nil && !errors.Is(err, migrate.ErrNoChange) {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
+
+	logger.Info("database migrations applied")
+
+	templateCache, err := newTemplateCache()
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
+
+	formDecoder := form.NewDecoder()
+
+	app := &appConfig{
+		logger:        logger,
+		tasks:         &models.TaskModel{DB: db},
+		templateCache: templateCache,
+		formDecoder:   formDecoder,
+	}
+
+	srv := &http.Server{
+		Addr:         *addr,
+		Handler:      app.routes(),
+		ErrorLog:     slog.NewLogLogger(logger.Handler(), slog.LevelError),
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+	}
+
+	logger.Info("starting server", "addr", srv.Addr)
+
+	err = srv.ListenAndServe()
+	logger.Error(err.Error())
+	os.Exit(1)
+}
+
+func getDSN() string {
+	dsnFlag := flag.String("dsn", "", "PostgreSQL data source name")
+	flag.Parse()
+	if *dsnFlag != "" {
+		return *dsnFlag
+	}
+	return os.Getenv("DSN")
+}
+
+func connectDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("postgres", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	err = db.PingContext(ctx)
+	if err != nil {
+		db.Close()
+		return nil, err
+	}
+
+	return db, nil
+}
